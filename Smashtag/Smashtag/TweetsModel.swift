9a3e560e5b29a0e74b3ed4ce0a0b52bea90c2f72
Egor Kolyshkin <egor.kolyshkin@mail.ru>
2017-10-06 15:45:14 +0300

    Added database for model

---
+++ Smashtag/Smashtag/TweetsModel.swift
@@ -0,0 +1,29 @@
+//
+//  TweetsModel.swift
+//  Smashtag
+//
+//  Created by Егор on 06.10.17.
+//  Copyright © 2017 Stanford University. All rights reserved.
+//
+
+import Foundation
+
+class TweetsModel {
+    
+    private static let defaults = UserDefaults.standard
+    private static let key = "RecentSearches"
+    private static let limit = 100
+    
+    static var searches: [String] = {
+        return (defaults.object(forKey: key) as? [String]) ?? []
+    }()
+    
+    static func add(_ search: String){
+        if !searches.contains(search)  {
+            searches.insert(search, at: 0)
+            if searches.count > limit {
+                search.remove(at: limit)
+            }
+        }
+    }
+}
